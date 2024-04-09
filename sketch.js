3b5bda3f8454afbacb8e4fe3e63d40baf47eb90f
BolzerLuca <matbunszey@gmail.com>
2024-04-09 08:57:07 +0200

    Kort trukket af bunken
    Kortet bliver derfter lagt som toppen af spillebunken.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let stack = [];
+let cardOnTable;
+let playedCard;
 
 function setup() {
   createCanvas(400, 400);
@@ -8,10 +10,29 @@ function setup() {
       stack.push(new kort(j, i));
     }
   }
+  NewCardOnTable();
   print(stack);
 }
 
 function draw() {
   background(220);
   
+}
+
+function NewCardOnTable() {
+  let tempRandomFromStack = int(random(0, stack.length));
+  cardOnTable = stack[tempRandomFromStack];
+  stack.splice(tempRandomFromStack, 1);
+}
+
+function mousePressed() {
+  // TODO: Lav ting ting der bestemmer hvilket kort man trykker på
+  // TODO: Når fejlkort gives, check om stack er tom og fyld i det tilfælde
+  
+}
+
+function valueMatching() {
+  if (playedCard == cardOnTable) {
+    return true;
+  }
 }
