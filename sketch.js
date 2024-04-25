bfc28090ebb5ba1dd27d76ba295729d2918cc7fa
Patrick47848448 <patr8997@edu.mercantec.dk>
2024-04-25 09:58:05 +0200

    fillHand, fillStack, myHand
    Lavet fillHand, puttet stacken ind i en fillStack function, lavet en myHand som kan fyldes med fillHand, har tegent kort i hånden.

--- sketch.js
+++ sketch.js
@@ -1,38 +1,61 @@
-let bg;
-let kA;
+let baggrund;
+let cardArray;
 let myHand = [];
-function preload(){
-  bg = loadImage('Assets//wood-Texture.png');
-  kA = loadImage('Assets//kort-Array.png');
-  // 81 & 118
-}
 let stack = [];
 let cardOnTable;
 let playedCard;
+let cardSizeX = 81;
+let cardSizeY = 117;
+
+function preload(){
+  baggrund = loadImage('Assets//wood-Texture.png');
+  cardArray = loadImage('Assets//kort-Array.png');
+  // 81 & 117
+}
+
 
 function setup() {
-
-  for (let j = 1; j < 5; j++) {
-    for (let i = 1; i < 14; i++) {
-      stack.push(new kort(j, i));
-    }
-  }
   NewCardOnTable();
-  print(stack);
   createCanvas(1000, 700);
-  background(bg);
-  for (let i = 0; i < 7; i++){
-    let temp = round(random(0,stack.length));
-    console.log(temp)
-    myHand.push(stack[temp]);
-    stack.splice(temp,1);
-  }
-  console.log(stack)
-  console.log(myHand)
+  background(baggrund);
+  fillStack();
+  fillHand();
+  kysseMysse();
+  print(stack);
+  print(myHand);
 }
 
 function draw() {
-//image(kA,width/2,height/2,81,118,myHand[1]*81,myHand[1]*118)
+//image(cardArray,width/2,height/2,81,118,myHand[1]*81,myHand[1]*118)
+}
+
+function kysseMysse() {
+  for (let j = 0; j < 2; j++){
+    for (let i = 0; i < 7; i++){
+      if(myHand[i+7*j]===undefined){
+        break
+      }
+      image(cardArray,width/4+i*cardSizeX,height-cardSizeY*2+j*cardSizeY,cardSizeX,cardSizeY,myHand[i+7*j].værdi*cardSizeX,myHand[i+7*j].kulør*cardSizeY,cardSizeX,cardSizeY);
+    
+    }
+  }
+}
+
+function fillStack() {
+  for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 13; i++) {
+      stack.push(new kort(j, i));
+    }
+  }
+}
+
+function fillHand() {
+  for (let i = 0; i < 7; i++){
+    let tempCard = floor(random(0,stack.length));
+    myHand.push(stack[tempCard]);
+    stack.splice(tempCard,1);
+  
+  }
 }
 
 function NewCardOnTable() {
